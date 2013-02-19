ae6061f09f0d898f61ce74f21cb31e02be9fbcd8
jdiez17 <jose.manuel.diez@gmail.com>
2013-02-19 21:18:41 +0100

    go fmt

--- oauth.go
+++ oauth.go
@@ -1,48 +1,47 @@
 package twitterstream
 
 import (
-    "github.com/mrjones/oauth"
+	"github.com/mrjones/oauth"
 )
 
 const (
-    k_auth_url = "https://api.twitter.com/oauth/authorize"
-    k_token_url = "https://api.twitter.com/oauth/request_token"
-    k_access_token_url = "https://api.twitter.com/oauth/access_token"
+	k_auth_url         = "https://api.twitter.com/oauth/authorize"
+	k_token_url        = "https://api.twitter.com/oauth/request_token"
+	k_access_token_url = "https://api.twitter.com/oauth/access_token"
 )
 
 type Oauth struct {
-    Consumer *oauth.Consumer
+	Consumer *oauth.Consumer
 }
 
 type AuthenticationRequest struct {
-    RequestToken *oauth.RequestToken
-    Url string
+	RequestToken *oauth.RequestToken
+	Url          string
 }
 
 func NewOauth(ConsumerKey, ConsumerSecret string) Oauth {
-    c := oauth.NewConsumer(
-        ConsumerKey,
-        ConsumerSecret,
-        oauth.ServiceProvider{
-            RequestTokenUrl: k_token_url,
-            AuthorizeTokenUrl: k_auth_url,
-            AccessTokenUrl: k_access_token_url,
-       })
-            
-    return Oauth{c}
-} 
+	c := oauth.NewConsumer(
+		ConsumerKey,
+		ConsumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   k_token_url,
+			AuthorizeTokenUrl: k_auth_url,
+			AccessTokenUrl:    k_access_token_url,
+		})
+
+	return Oauth{c}
+}
 
 func (o Oauth) NewAuthenticationRequest() (*AuthenticationRequest, error) {
-    requestToken, url, err := o.Consumer.GetRequestTokenAndUrl("oob")
-    if err != nil {
-        return nil, err
-    }
+	requestToken, url, err := o.Consumer.GetRequestTokenAndUrl("oob")
+	if err != nil {
+		return nil, err
+	}
 
-    return &AuthenticationRequest{requestToken, url}, nil
+	return &AuthenticationRequest{requestToken, url}, nil
 }
 
 func (o Oauth) GetAccessToken(RequestToken *oauth.RequestToken, code string) (*oauth.AccessToken, error) {
-    accessToken, err := o.Consumer.AuthorizeToken(RequestToken, code)
-    return accessToken, err
+	accessToken, err := o.Consumer.AuthorizeToken(RequestToken, code)
+	return accessToken, err
 }
-
--- twitter.go
+++ twitter.go
@@ -1,59 +1,58 @@
 package twitterstream
 
 import (
-    "github.com/mrjones/oauth"
-    "bufio"
-    "encoding/json"
-    "fmt"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/mrjones/oauth"
 )
 
 type User struct {
-    Id uint64
-    Screen_name string
-    Name string
+	Id          uint64
+	Screen_name string
+	Name        string
 }
 
 type Tweet struct {
-    Id uint64
-    User User
-    Text string
+	Id   uint64
+	User User
+	Text string
 }
 
 const k_userstream = "https://userstream.twitter.com/2/user.json"
+
 //const k_userstream = "https://stream.twitter.com/1.1/statuses/sample.json"
 
 func DoUserStream(oauth Oauth, accessToken *oauth.AccessToken) (<-chan *Tweet, error) {
-    tweets := make(chan *Tweet)
-    response, err := oauth.Consumer.Get(k_userstream, map[string]string{}, accessToken)
-    
-    if err != nil { return nil, err }
-    go func() {
-        defer close(tweets)
-        defer response.Body.Close()
-        reader := bufio.NewReader(response.Body)
-        
-        for {
-            buf, err := reader.ReadBytes('\n')
-            if err != nil {
-                panic(err)
-            }
+	tweets := make(chan *Tweet)
+	response, err := oauth.Consumer.Get(k_userstream, map[string]string{}, accessToken)
 
-            tweet := new(Tweet)
-            
-            err = json.Unmarshal(buf, tweet)
-            if err != nil {
-                fmt.Println(err.Error())
-                continue
-            }
-            if tweet.Id != 0 {
-                tweets <- tweet
-            }
-        }
-    }()
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer close(tweets)
+		defer response.Body.Close()
+		reader := bufio.NewReader(response.Body)
 
-    return tweets, nil
+		for {
+			buf, err := reader.ReadBytes('\n')
+			if err != nil {
+				panic(err)
+			}
+
+			tweet := new(Tweet)
+
+			err = json.Unmarshal(buf, tweet)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			if tweet.Id != 0 {
+				tweets <- tweet
+			}
+		}
+	}()
+
+	return tweets, nil
 }
-    
-    
-
-
